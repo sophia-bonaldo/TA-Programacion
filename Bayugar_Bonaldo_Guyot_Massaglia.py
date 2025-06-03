4eac20938c5174e9f488a4071142b3ba441243d7
Sofía Mailén Grasso Bonaldo <sgrassobonaldo@udesa.edu.ar>
2025-06-03 15:26:31 -0300

    Update Bayugar_Bonaldo_Guyot_Massaglia.py
    Atributos clase Votante

--- Bayugar_Bonaldo_Guyot_Massaglia.py
+++ Bayugar_Bonaldo_Guyot_Massaglia.py
@@ -18,33 +18,38 @@ df = pd.read_csv('Decision_Voto_Elecciones.csv')
 
 ## Clases y Objetos
 class Votante :
-    def __init__(self, id_votante, genero, edad, circunscripcion, nivel_socioeconomico, nivel_educativo, afiliacion_politica, interes_politica) :
+    def __init__(self, id_votante, genero, edad, circunscripcion, nivel_socioeconomico, nivel_educativo, afiliacion_politica, interes_politica, preocupacion_economica, preocupacion_seguridad, opinion_gobierno_actual, percepcion_corrupcion, intencion_voto, dispocision_cambiar_voto, participacion_voto_anterior) :
         #Genérico
         self.id_votante = id_votante
         self.genero = genero
         self.edad = edad
         
         #Situación individual
-        self.circunscripcion
-        self.nivel_socioeconomico
-        self.nivel_educativo
+        self.circunscripcion = circunscripcion
+        self.nivel_socioeconomico = nivel_socioeconomico
+        self.nivel_educativo = nivel_educativo
         
         #Partidarios
-        self.afiliacion_politica
-        self.interes_politica
+        self.afiliacion_politica = afiliacion_politica
+        self.interes_politica = interes_politica
         
         #Opinión y Preocupación
-        self.preocupacion_economica
-        self.preocupacion_seguridad
+        self.preocupacion_economica = preocupacion_economica 
+        self.preocupacion_seguridad = preocupacion_seguridad
         
-        self.opinion_gobierno_actual
-        self.percepcion_corrupcion
+        self.opinion_gobierno_actual = opinion_gobierno_actual
+        self.percepcion_corrupcion = percepcion_corrupcion
         
         #Sobre el Voto
-        self.intencion_voto
-        self.dispocision_cambiar_voto
-        self.participacion_voto_anterior
-
+        self.intencion_voto = intencion_voto
+        self.dispocision_cambiar_voto = dispocision_cambiar_voto
+        self.participacion_voto_anterior = participacion_voto_anterior
+        
+        
+        #MÉTODOS
+        ...
+        
+        
 class Partido :
     ...
 
